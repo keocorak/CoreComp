1ec27fa8aa25cd4010c36c98094c2c2407a0ca7a
Keo Corak <corak@wisc.edu>
2020-12-07 10:47:36 -0600

    updated tests

--- tests/testthat/test-ENE.R
+++ tests/testthat/test-ENE.R
@@ -1,9 +1,8 @@
 
 test_that("ENE is a number", {
   t<-matrix(sample(1:1000, 100), nrow=10, ncol=10)
-  colnames(t)<-sample(letters,10)
+  colnames(t)<-rownames(t)<-sample(letters,10)
 
-
-  expect_length(ENE(t, sample(colnames(t),4)),1)
+  expect_length(ENE(t,sample(colnames(t),4)),1)
   expect_type(ENE(t,sample(colnames(t),4)), "double")
 })
