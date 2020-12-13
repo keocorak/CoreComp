5d71d2d44dad47050868bd940aa893a8866a902b
Keo Corak <corak@wisc.edu>
2020-12-13 10:19:21 -0600

    updated params and replaced %like% with grep

--- R/pheno.R
+++ R/pheno.R
@@ -2,19 +2,20 @@
 #'
 #' Calculate least squares means for experimental phenotype data
 #'
-#' @param x data table
-#' @param id.vars numeric vector corresponding to columns in data
-#' @param measure.vars numeric vector corresponding to columns in data
+#'
+#'
+#' @param x a data frame containing entry names,
+#' @param id.vars numeric vector corresponding to ID variables columns in data
+#' @param measure.vars numeric vector corresponding to measured variables columns in data
 #' @param form model formula, for example "value~-1+entryID+(1|rep)"
 #' @param uniquecol integer corresponding to entry names column- will be included in data frame output
-#' @param like character string that indicates that something is an entry
-#' @param alpha confidence level for confidence intervals
-#'
-#' @return Rdata files of lsmeans and confidence intervals
+#' @param out data to return. Should be either "means" for least-squares means estimates or "ci" for confidence intervals.
+#' @param alpha significance level used to construct confidence intervals. Only used when out="ci"
+#' @return dataframe of means or confidence intervals
 #' @export
 #'
 #'
-lsmeans<-function(x,id.vars, measure.vars, form, uniquecol, like,  alpha=0.05){
+lsmeans<-function(x,id.vars, measure.vars, form, uniquecol, out="means", alpha=NULL){
 
   #thanks to guilliame ramstein for most of this code. I just put it into a function
 
@@ -35,14 +36,21 @@ lsmeans<-function(x,id.vars, measure.vars, form, uniquecol, like,  alpha=0.05){
     tmp$value<-as.numeric(tmp$value)
     form<-stats::as.formula(form)
     fit<-lme4::lmer(form, data=tmp)
-    blues<-lme4::fixef(fit, add.dropped=T)[grep(like, names(lme4::fixef(fit)))]
+    blues<-lme4::fixef(fit, add.dropped=T)[grep(colnames(x)[uniquecol], names(lme4::fixef(fit)))]
     blues.frame[levels(fit@frame[[2]]), trait]<-blues
+  }
 
-    se_blues <- stats::coef(summary(fit))[grep(like, names(lme4::fixef(fit))), "Std. Error"]
+  if(out=="means"){
+    return(blues.frame)
+  } else if(out=="ci") {
+    se_blues <- stats::coef(summary(fit))[grep(colnames(x)[uniquecol], names(lme4::fixef(fit))), "Std. Error"]
     edf <- sum(stats::hatvalues(fit))
     ci <- matrix(rep(blues, 2), ncol=2) + (stats::qt(p=1-alpha/2, df=edf) * se_blues) %*% t(c(-1, 1))
     ci.array[levels(fit@frame[[2]]), trait, ] <- ci
+    return(ci.array)
   }
-  saveRDS(blues.frame, file="lsmeans.Rdata")
-  saveRDS(ci.array, file="ci.Rdata")
 }
+
+
+
+
